372a63bcf326f7180fa7f072d3d3b30eff3b14e6
Md. Kamrul Hasan <55704250+kamrul15-8841@users.noreply.github.com>
2022-08-07 21:08:42 +0600

    commit
    done

---
+++ src/App.vue
@@ -0,0 +1,301 @@
+<template>
+  <div id="app">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#">Md. Kamrul Hasan</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+          <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav text-uppercase ml-auto">
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#education">Education</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#skills">Skills</a>
+            </li>
+           
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Header -->
+    <header class="masthead">
+      <div class="container">
+        <div class="intro-text">
+          <div class="intro-lead-in">Welcome to My Personal Prortfolio</div>
+          <div class="intro-heading text-uppercase">It's Nice To Meet You</div>
+          <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#education">Tell Me More</a>
+        </div>
+      </div>
+    </header>
+
+    <!-- Education -->
+    <section id="education">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <h2 class="section-heading text-uppercase">Education</h2>
+            <h3 class="section-subheading text-muted">My Educational Background.</h3>
+          </div>
+        </div>
+        <div class="row text-center">
+          <div class="col-md-4">
+            <span class="fa-stack fa-4x">
+              <i class="fas fa-circle fa-stack-2x text-primary"></i>
+              <i class="fas fa-shopping-cart fa-stack-1x fa-inverse"></i>
+            </span>
+            <h4 class="service-heading">Bachelor of Science BSc in Computer Science and Engineering</h4>
+            <p class="text-muted">Daffodil International University, Dhaka <br> January 2017 - March 2021 <br>
+                                       •	CGPA - 3.83 Out of 4.00  <br> </p>
+                                       
+
+          </div>
+          <div class="col-md-4">
+            <span class="fa-stack fa-4x">
+              <i class="fas fa-circle fa-stack-2x text-primary"></i>
+              <i class="fas fa-laptop fa-stack-1x fa-inverse"></i>
+            </span>
+             <h4 class="service-heading">Higher Secondary Certificate (HSC)/ Science </h4>
+            <p class="text-muted">Bogura Cantonment Public School and College, Bogura<br> 2014 - 2016  </p>
+          </div>
+          <div class="col-md-4">
+            <span class="fa-stack fa-4x">
+              <i class="fas fa-circle fa-stack-2x text-primary"></i>
+              <i class="fas fa-lock fa-stack-1x fa-inverse"></i>
+            </span>
+             <h4 class="service-heading">Secondary School Certificate (Dakhil)/ Science </h4>
+            <p class="text-muted">Bar Anjul Darul Aman Dakhil Madrasah, Bogura <br>
+                                       2012 - 2013 <br> </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Portfolio Grid -->
+    <PortfolioCo></PortfolioCo>
+
+    <!-- Skills -->
+    <section id="skills">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <h2 class="section-heading text-uppercase">Skills</h2>
+            <h3 class="section-subheading text-muted">My Skils and knowledges </h3>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-12">
+            <ul class="timeline">
+              <li>
+                <div class="timeline-image">
+                  <img class="rounded-circle img-fluid" src="img/about/1.jpg" alt="">
+                </div>
+                <div class="timeline-panel">
+                  <div class="timeline-heading">
+                    <h4>SOFT SKILLS</h4>
+                    <h4 class="subheading">Some of my Soft Skills</h4>
+                  </div>
+                  <div class="timeline-body">
+                    <p class="text-muted">•	Adaptability <br>
+                                          •	Collaboration <br>
+                                          •	Strong Work Ethic <br>
+                                          •	Problem Solving <br>
+                                          •	Punctuality
+
+</p>
+                  </div>
+                </div>
+              </li>
+              <li class="timeline-inverted">
+                <div class="timeline-image">
+                  <img class="rounded-circle img-fluid" src="img/about/2.jpg" alt="">
+                </div>
+                <div class="timeline-panel">
+                  <div class="timeline-heading">
+                    <h4>HARD SKILLS</h4>
+                    <h4 class="subheading">Some of my Hard Skills</h4>
+                  </div>
+                  <div class="timeline-body">
+                    <p class="text-muted">Web Development <br> Full Stack Development <br> Android Development
+
+                    </p>
+                  </div>
+                </div>
+              </li>
+              <li>
+                <div class="timeline-image">
+                  <img class="rounded-circle img-fluid" src="img/about/3.jpg" alt="">
+                </div>
+                <div class="timeline-panel">
+                  <div class="timeline-heading">
+                    <h4>Programming Languages </h4>
+                    <h4 class="subheading">I Know Some Programming Language</h4>
+                  </div>
+                  <div class="timeline-body">
+                    <p class="text-muted">•	Java <br>
+                                          •	Python  <br>
+                                          •	C#, C & C++ <br>
+                                          •	.NET, Django & Bootstrap  
+</p>
+                  </div>
+                </div>
+              </li>
+              <li class="timeline-inverted">
+                <div class="timeline-image">
+                  <img class="rounded-circle img-fluid" src="img/about/4.jpg" alt="">
+                </div>
+                <div class="timeline-panel">
+                  <div class="timeline-heading">
+                    <h4>Web Development </h4>
+                    <h4 class="subheading">What I use in Web Development</h4>
+                  </div>
+                  <div class="timeline-body">
+                    <p class="text-muted">•	HTML <br>
+                                          •	CSS <br>
+                                          •	JavaScript
+</p>
+                  </div>
+                </div>
+              </li>
+              <li class="timeline-inverted">
+                <div class="timeline-image">
+                  <h4>I 
+                    <br>Want to be
+                    <br>Software Engineer</h4>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+
+  
+   
+
+    <!-- Contact -->
+    <section id="contact">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <h2 class="section-heading text-uppercase">Contact Me</h2>
+            
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-12">
+            <form id="contactForm" name="sentMessage" novalidate="novalidate">
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <input class="form-control" id="name" type="text" placeholder="Your Name *" required="required" data-validation-required-message="Please enter your name.">
+                    <p class="help-block text-danger"></p>
+                  </div>
+                  <div class="form-group">
+                    <input class="form-control" id="email" type="email" placeholder="Your Email *" required="required" data-validation-required-message="Please enter your email address.">
+                    <p class="help-block text-danger"></p>
+                  </div>
+                  <div class="form-group">
+                    <input class="form-control" id="phone" type="tel" placeholder="Your Phone *" required="required" data-validation-required-message="Please enter your phone number.">
+                    <p class="help-block text-danger"></p>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <textarea class="form-control" id="message" placeholder="Your Message *" required="required" data-validation-required-message="Please enter a message."></textarea>
+                    <p class="help-block text-danger"></p>
+                  </div>
+                </div>
+                <div class="clearfix"></div>
+                <div class="col-lg-12 text-center">
+                  <div id="success"></div>
+                  <button id="sendMessageButton" class="btn btn-primary btn-xl text-uppercase" type="submit">Send Message</button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Footer -->
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <span class="copyright">Copyright &copy; Your Website 2018</span>
+          </div>
+          <div class="col-md-4">
+            <ul class="list-inline social-buttons">
+              <li class="list-inline-item">
+                <a href="#">
+                  <i class="fab fa-twitter"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="#">
+                  <i class="fab fa-facebook-f"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="#">
+                  <i class="fab fa-linkedin-in"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-md-4">
+            <ul class="list-inline quicklinks">
+              <li class="list-inline-item">
+                <a href="#">Privacy Policy</a>
+              </li>
+              <li class="list-inline-item">
+                <a href="#">Terms of Use</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <!-- Portfolio Modals -->
+ </div>
+</template>
+
+<script>
+
+import PortfolioCo from './components/PortfolioCo.vue'
+
+export default {
+  name: 'app',
+  components: {
+    PortfolioCo,
+  
+}
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #36689b;
+}
+
+body {
+  background: black;
+}
+</style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,69 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+      <div class="container">
+        <router-link class="navbar-brand js-scroll-trigger" to="/page-top">Md. Kamrul Hasan</router-link>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+          <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav text-uppercase ml-auto">
+            <li
+                class="nav-item"
+                v-for="(linkObj, ind) in navList"
+                :key="ind"
+            >
+              <a
+                class="nav-link js-scroll-trigger"
+                :href="linkObj.path"
+                v-if="linkObj.path.indexOf('#') === 0"
+            >{{ linkObj.name }}</a>
+            <router-link
+                class="nav-link js-scroll-trigger"
+                v-else
+                :to="linkObj.path"
+            >{{ linkObj.name }}
+            </router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        navList: [
+          
+            {
+                name: 'Education',
+                path: '#education'
+            },
+            {
+                name: 'Portfolio',
+                path: '#portfolio'
+            },
+            {
+                name: 'About',
+                path: '#about'
+            },
+         
+            {
+                name: 'Contact',
+                path: '#contact'
+            },
+           
+        ]
+    })
+}
+</script>
+
+<style scoped>
+
+#mainNav.navbar {
+    background: rgba(0,0,0,0.5);
+}
+
+
+</style>
